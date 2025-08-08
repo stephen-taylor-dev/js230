a3fb07d5995200a0655be793cf5751d35df72c70
Stephen Taylor <stephenmtaylor93@gmail.com>
2025-08-08 17:32:14 -0400

    more stuff

---
+++ polar_bear.js
@@ -0,0 +1,31 @@
+const wordCounts = [...document.querySelectorAll('h2')].map(node => {
+   return node.textContent.split(' ').length; 
+});
+
+document.querySelector('.toc');
+document.querySelector('#toc');
+document.getElementById('toc')
+document.getElementsByClassName('toc')[0];
+
+
+
+[...document.querySelectorAll('.toc a')].forEach((node, idx) => {
+    if (idx % 2 === 1) {
+        node.style.color = 'green';
+    }
+});
+
+[...document.querySelectorAll('.thumbcaption')].map(node => node.textContent.trim())
+
+let tableRows = document.querySelector('.infobox').children[0].children;
+
+// Get elements inside table that are part of scientific classification
+[...document.querySelector('.infobox').children[0].children]
+  .map(node => node.children)
+  .filter(collections => collections.length === 2)
+  .reduce((obj, nodes) => {
+    let key = nodes[0].textContent.trim().slice(0, -1);
+    let val = nodes[1].textContent.trim();
+    obj[key] = val;
+    return obj;
+  }, {});
